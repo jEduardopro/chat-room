cf8243853c2ea79fc77f83c385f835237f9560f9
Jesus Eduardo <lalo.codein22@gmail.com>
2023-09-14 17:50:48 -0600

    config socket.io connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,29 @@
 import express from 'express'
+import http from 'http'
+import {Server} from 'socket.io'
 import cors from 'cors'
+
+const port = process.env.PORT || 3000;
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-const port = process.env.PORT || 3000;
+const httpServer = http.createServer(app)
+const io = new Server(httpServer)
+
+app.get('/', (req, res) => {
+	res.json({ msg: 'Hello world from socket' })
+})
+
+io.on('connection', (socket) => {
+	console.log('Connection is ready');
+})
 
 const main = async () => {
 	try {
 		
-		app.listen(port, () => {
+		httpServer.listen(port, () => {
 			console.log(`Server running on port ${port}`)
 		})
 

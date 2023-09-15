c74d5e59f82148cf4e383d99faf650fff607c24d
Jesus Eduardo <lalo.codein22@gmail.com>
2023-09-15 10:13:40 -0600

    define models

---
+++ server/src/models/Chat.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+const ChatSchema = new mongoose.Schema({
+	chatName: { type: String, trim: true },
+	users: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User'
+		}
+	],
+	lastestMessage: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Message'
+	},
+}, {
+	timestamps: true
+})
+
+const Chat = mongoose.model('Chat', ChatSchema)
+
+module.exports = Chat
